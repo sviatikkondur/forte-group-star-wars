f9026e78bd31bdb2457ed9077aa69ff854b2ac9d
Sviatoslav Kondur <107207978+sviatikkondur@users.noreply.github.com>
2024-01-14 11:21:36 +0100

    add not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Box, Button, Typography } from '@mui/material'
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 
-export const NotFoundPage = () => {
-  return (
-    <div style={{color: 'white'}}>NotFoundPage</div>
-  )
+export const NotFoundPage: React.FC = () => {
+    const navigate = useNavigate()
+
+    const redirectToHomePage = () => {
+        navigate('/')
+    }
+
+    return (
+        <Box
+        position={'relative'}
+        width={'100%'}
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        flexDirection={'column'}
+        >
+            <Typography fontSize={'4em'}>Oops 404!</Typography>
+            <Box 
+              onClick={() => redirectToHomePage()}
+              display={'flex'}
+              alignItems={'center'}
+              gap={1}
+              border={'1px solid #fff'}
+              borderRadius={5}
+              padding={2}
+              sx={{
+                backgroundColor: '#000'
+              }}
+            >
+              <RocketLaunchIcon />
+                <Typography
+                  variant='h5'
+                >
+                  Fly Home
+                </Typography>
+            </Box>
+        </Box>
+    )
 }
+
